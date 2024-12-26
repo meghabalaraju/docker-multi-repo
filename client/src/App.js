6389e5eb5154fbef33d36799ab9b5843ac16f429
meghabalaraju <meghabalaraju@gmail.com>
2024-12-26 16:48:58 +0100

    add new files for various node modules and configurations

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import OtherPage from "./OtherPage";
 import Fib from "./Fib";
 
